4e47144f6a0dee57c8d643c3a34e123fa24273ea
SPiRAL小橋 <kobashi@spiral-ltd.co.jp>
2022-08-16 17:31:20 +0900

    課題リスト2の表示
    課題のリスト2-1を表示できるようにしました。
    ファイルを1つ追加しています。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import List1 from './List1';
+import List2 from './List2';
 
 function App() {
   return (
     <React.Fragment>
+      リスト2
+      <List2 />
       リスト1
       <List1 />
     </React.Fragment>
