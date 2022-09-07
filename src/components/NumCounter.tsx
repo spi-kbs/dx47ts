57fa7fbca891e38bb52c00a8bae982f45b9f0559
SPiRAL小橋 <kobashi@spiral-ltd.co.jp>
2022-09-07 16:27:26 +0900

    リセット/入力(途中)
    リセットボタンを追加しました。
    直接入力は途中です。

--- src/components/NumCounter.tsx
+++ src/components/NumCounter.tsx
@@ -4,34 +4,39 @@ import {Region} from "../types/Prefecture"
 
 export const NumCounter = () => {
   const [number, setNumber] = useState<number>(0);
-  const [error, setError] = useState<string>('');
+  // const [error, setError] = useState<string>('');
 
   return (
     <div>
-      <p>{number}{error}</p>
-      <p></p>
+      <p>{number}</p><Error num={number}/>
       <button onClick={
         () => {
           setNumber(number + 1)
-          setError('')
+          // setError('')
         }
       }>+</button>
       <button onClick={
         () => {
           const nextValue = number - 1
-          if (nextValue < 0) {
-            setError(' : error!')
-            return;
-          }
+          // if (nextValue < 0) {
+          //   // setError(' : error!')
+          //   return;
+          // }
           setNumber(nextValue)
-          setNumber(Math.max(0, number - 1))
+          // setNumber(Math.max(0, number - 1))
         }
       }>-</button>
+      <p><button onClick={()=>setNumber(0)}>reset</button></p>
+      <p><input type="text" value={number}/><button onClick={()=>setNumber(number)}>set</button></p>
     </div>
   )
 };
 
-
+const Error = ({num}: {num: number}) => {
+  return num < 0 ? (
+    <p>error!</p>
+  ) : null;
+}
 // export const Number = () => {
 //   return (
 //     <></>
